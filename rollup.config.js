c69ba6e5d85cf83e3823c50b2c85344f5ad6efb4
Ondrej Zara <ondrej.zara@firma.seznam.cz>
2016-09-15 15:21:31 +0200

    quotes

--- rollup.config.js
+++ rollup.config.js
@@ -3,6 +3,6 @@ import includePaths from "rollup-plugin-includepaths";
 export default {
     entry: "src/app.js",
     format: "iife",
-    dest: 'app.js',
+    dest: "app.js",
     plugins: [ includePaths() ]
 };
